6b05852ea768de2838aa09e44d3d21bf3d8b2e89
mzyzc <mzyzc@czyz.xyz>
2020-12-11 10:48:41 +0000

    Added dotfiles download script

---
+++ get-dotfiles.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd $HOME
+git clone https://github.com/mzyzc/dotfiles.git
+git submodule update --init --recursive
--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+
+cd $(dirname $0)
+
 timedatectl set-ntp true && echo "System time synchronized!"
+
+./install-pkgs.sh
+./improve-fonts.sh
+./get-dotfiles.sh
