bc2112a825d7764b350f323b81339651795ac07a
mzyzc <mzyzc@czyz.xyz>
2020-12-25 21:11:29 +0000

    Condensed script into one file

--- get-dotfiles.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd $HOME
-git clone https://github.com/mzyzc/dotfiles.git
-git submodule update --init --recursive
--- improve-fonts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd /etc/fonts/conf.d
-ln -s ../conf.avail/10-sub-pixel-rgb.conf
-ln -s ../conf.avail/11-lcdfilter-default.conf
-ln -s ../conf.avail/70-no-bitmaps.conf
